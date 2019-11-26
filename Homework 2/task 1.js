808363e16252a2c4b47a5e73a080aae06a68d96b
VitekTennisist1256 <ryzhovryjov348@gmail.com>
2019-11-26 03:28:09 +0200

    today

--- Homework 2/task 1.js
+++ Homework 2/task 1.js
@@ -48,19 +48,18 @@ var esd = [];
   }
 ];
 // Задача "Вывести пользователей у которых имя или фамилия начинается на букву E"
-let E = 'E';
-var arr = [];
-function charE(last_name){
-     last_name.split('');
-     return last_name[0]===E ?
-    arr.push(last_name):null
-}
-const result = users.filter(function(obj) {
-         return charE(obj.first_name);
-     });
-const result1 = users.filter(function(obj) {
-  return charE(obj.last_name);
-console.log(arr);
+function ucFirst(str) {
+    return str.charAt(0).toLocaleUpperCase() + str.slice(1);
+  }
+ 
+ var bigChar = users.map(function(obj) {
+     obj.first_name = ucFirst(obj.first_name);
+     obj.last_name = ucFirst(obj.last_name);
+     return obj;
+ });
+ 
+ console.log(bigChar);
+ 
 
 // Задача Отобразить пользователей которым более 30 лет 
 let minAge = 30;
